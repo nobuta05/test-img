f206b86d5649f05d964ecec96ffd1197edf45270
nobuta05 <10789600+nobuta05@users.noreply.github.com>
2023-03-23 19:43:59 +0900

    実行条件の確認 (#1)
    * added process
    
    * modified
    
    * modified so that to execute CI when pushing to main
    
    * minor changed
    
    * added img
    
    * added font

--- img.py
+++ img.py
@@ -1,11 +1,20 @@
 from diagrams import Cluster, Diagram
 from diagrams.onprem.compute import Server
 
-with Diagram("Servers @ Lab", show=False, filename="servers_@_lab"):
+with Diagram("LDAP @ Lab", show=False, filename="./outputs/ldap_@_lab"):
     with Cluster("LDAP Clients"):
-        ldap_clients = [
+        ldap_clients = [Server("GPU serv"), Server("CPU serv")]
+
+    ldap_clients >> Server("LDAP Serv, Workstation")
+
+with Diagram(
+    "Servers @ Lab", show=False, filename="./outputs/servers_@_lab", direction="TB"
+):
+    with Cluster("Lab"):
+        clients = [
+            Server("Workstation\nxx.xx.xx.xx"),
             Server("GPU serv"),
-            Server("CPU serv")
+            Server("CPU serv"),
         ]
 
-    ldap_clients >> Server("Workstation")
+    Server("所内ルータ") - clients
